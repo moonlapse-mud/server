618571d9fe3079b8191e77b17dbec4f539d44939
Corey Batchler <coreybatchler7@gmail.com>
2022-06-05 07:45:22 +1000

    changed timer workings

--- protocol.py
+++ protocol.py
@@ -7,13 +7,25 @@ class Protocol:
         self.server = server
         self.socket: socket.socket = sock
 
+    def disconnect(self, reason="graceful"):
+        print(f"{self} disconnected. Reason: {reason}")
+        self.server.selector.unregister(self)
+        self.server.protocols.pop(self.fileno())
+
     def read(self):
-        # todo: error handling
-        bs = self.socket.recv(4)     # read header bytes
-        header = Header.from_bytes(bs)
-        data = self.socket.recv(header.length)
-        p = from_bytes(bs + data, self.server.privkey)
-        print(f"{self} received {p}")
+        try:
+            bs = self.socket.recv(4)     # read header bytes
+            if not bs:
+                # disconnected
+                self.disconnect()
+                return
+
+            header = Header.from_bytes(bs)
+            data = self.socket.recv(header.length)
+            p = from_bytes(bs + data, self.server.privkey)
+            print(f"{self} received {p}")
+        except Exception:
+            print(f"{self} received some bytes, but they were not well formed.")
 
     def fileno(self):
         return self.socket.fileno()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from selectors import *
 import os
 import sys
 import threading
+import time
 from protocol import *
 import socket
 from moonlapseshared import crypto
@@ -11,42 +12,46 @@ PORT = 42523
 
 
 class Server:
-
     def __init__(self):
         self.selector = DefaultSelector()
         self.protocols = {}     # map of socket FD : Protocol
         self.socket = None
         self.pipe_r, self.pipe_w = os.pipe()    # pipe used for timer
         self.pubkey, self.privkey = crypto.load_rsa_keypair(os.path.dirname(os.path.realpath(sys.argv[0])))
-        self.timer = None
+        self.timer = threading.Thread(target=self._timer_loop, daemon=True)
         self.tickrate = 0.5      # 20 ticks per second
 
     def start(self):
-        # todo: error handling
-        # set up listen socket
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(('', PORT))
-        self.socket.listen(1)
-        print(f"Moonlapse Server listening on {PORT}")
+        try:
+            # set up listen socket
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind(('', PORT))
+            self.socket.listen(1)
+            print(f"Moonlapse Server listening on {PORT}")
 
-        # set up tick timer
-        self.timer = threading.Timer(1.0 / self.tickrate, self._timer_callback)
-        self.timer.start()
+            # set up tick timer
+            self.timer.start()
 
-        self._main_loop()
+            self._main_loop()
+        except Exception as e:
+            print(f"Error setting up the server. {e}")
+            sys.exit(1)
 
-    def accept(self, sock: socket.socket):
-        print("Received new connection")
-        proto_s, _ = sock.accept()
-        proto = Protocol(self, proto_s)
-        self.protocols[proto_s.fileno()] = proto
-        self.selector.register(proto, EVENT_READ, self.read)
+    def _accept(self, sock: socket.socket):
+        try:
+            print("Received new connection")
+            proto_s, _ = sock.accept()
+            proto = Protocol(self, proto_s)
+            self.protocols[proto_s.fileno()] = proto
+            self.selector.register(proto, EVENT_READ, self._read)
+        except Exception as e:
+            print(f"Error during accept. {e}")
 
-    def read(self, proto: Protocol):
+    def _read(self, proto: Protocol):
         proto.read()
 
     def _main_loop(self):
-        self.selector.register(self.socket, EVENT_READ, self.accept)
+        self.selector.register(self.socket, EVENT_READ, self._accept)
         self.selector.register(self.pipe_r, EVENT_READ, self._tick)
 
         while True:
@@ -59,9 +64,7 @@ class Server:
         os.read(self.pipe_r, 1)
         print("Tick!")
 
-    def _timer_callback(self):
-        os.write(self.pipe_w, b'1')
-
-        # restarting the timer
-        self.timer = threading.Timer(1.0 / self.tickrate, self._timer_callback)
-        self.timer.start()
+    def _timer_loop(self):
+        while True:
+            time.sleep(1.0 / self.tickrate)
+            os.write(self.pipe_w, b'1')
